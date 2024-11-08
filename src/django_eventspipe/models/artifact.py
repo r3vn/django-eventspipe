69f346c5fca0c688d28ab5f620c1d5b8fc621b33
r3vn <giuseppe.corti@protonmail.com>
2024-11-08 12:35:41 +0100

    [models] Artifact: avoid race conditions

--- src/django_eventspipe/models/artifact.py
+++ src/django_eventspipe/models/artifact.py
@@ -1,23 +1,27 @@
 import hashlib
-from django.db import models
+from django.db import models, transaction
 
 class Artifact(models.Model):
-    data       = models.BinaryField(blank=True)
-    md5sum     = models.CharField(max_length=32, blank=True, editable=False, unique=True)  # Unique MD5 checksum
+    data = models.BinaryField(blank=True)
+    md5sum = models.CharField(max_length=32, blank=True, editable=False, unique=True)
 
     @classmethod
-    def get_or_create(cls, data:bytes) -> object:
+    def get_or_create(cls, data: bytes) -> "Artifact":
         # Compute the MD5 checksum of the data
         md5sum = hashlib.md5(data).hexdigest()
 
-        if cls.objects.filter(md5sum=md5sum).exists():
-            return cls.objects.get(md5sum=md5sum)
-
-        file = cls(data=data,md5sum=md5sum)
-        file.save()
-
-        return file
+        with transaction.atomic():
+            # Attempt to fetch the Artifact with a row lock
+            artifact = cls.objects.select_for_update().filter(md5sum=md5sum).first()
+            if artifact:
+                return artifact
+            
+            # If no existing Artifact, create and save a new one
+            artifact = cls(data=data, md5sum=md5sum)
+            artifact.save()
         
+        return artifact
+
     @property
     def size(self) -> float:
         """
